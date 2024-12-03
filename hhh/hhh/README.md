b74c7d0c21fb4b1ec7ac5691581e3cddb340eedb
ashy540 <muzofaashlea@gmail.com>
2024-12-03 22:40:57 +0200

    Add files via upload

---
+++ hhh/hhh/README.md
@@ -0,0 +1,4 @@
+# hhh
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/ASHLEA777/pen/KwPdgNR](https://codepen.io/ASHLEA777/pen/KwPdgNR).
+
---
+++ hhh/hhh/dist/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('scroll', function() {
+  const footer = document.querySelector('footer');
+  const footerPosition = footer.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight;
+  if (footerPosition < screenPosition) {
+    footer.classList.add('visible');
+  }
+});
+
+
+  // JavaScript to scroll to the contact form
+  document.getElementById('learnMoreBtn').addEventListener('click', function () {
+    const contactFormSection = document.getElementById('contactFormSection');
+    contactFormSection.scrollIntoView({ behavior: 'smooth' });
+  });
+
+
+  document.getElementById('getstarted').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+  });
